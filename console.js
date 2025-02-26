9a2923b096cfa63422c4e99a5c75722443901a9a
ThiyaguAntony <antonythiyagu29@gmail.com>
2025-02-26 12:28:26 +0530

    JavaScript Console
    In javascript, the console is an object which provides access to the browser debugging console. We can open a console in web browser. The console object provides us with several different methods :
    
    ⁕ log()
    ⁕ error()
    ⁕ warn()
    ⁕ clear()
    ⁕ time() and timeEnd()
    ⁕ table()
    ⁕ count()
    ⁕ custom console logs

---
+++ console.js
@@ -0,0 +1,38 @@
+// console log() with String
+console.log("Welcome to the Console ...!");
+
+// console log() with number
+console.log(20);
+
+// console log() with decimal
+console.log(12.5);
+
+// console log() with boolen
+console.log(true);
+
+// console log() with array
+console.log([20,30,10]);
+
+// console log() with object
+console.log({name: "Thiyagu", emp_id :"EID001"});
+
+// console table with object
+console.table([{name : "Thiyagu", emp_id:"EID0001"}, {name : "Antony", emp_id:"EID0002"}]);
+
+// console error()
+console.error("Mobile No is not Vaildated...!");
+
+// console warn()
+console.warn("Validate the Mobile No...!");
+
+// console clear()
+console.clear();
+
+// console time() && timeEnd();
+console.time("Timer 1");
+for(i=0;i<10000;i++){
+    console.log(i);
+}
+console.timeEnd("Timer 1");
+
+
