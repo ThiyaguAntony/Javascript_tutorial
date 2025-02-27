b4e3993555d9798000a5a31bf30f871daaf359ac
ThiyaguAntony <antonythiyagu29@gmail.com>
2025-02-28 00:27:46 +0530

    Arithmetic Operations in JavaScript
    ⁕ Addition (+): This operator is used to add two or more numbers together. For example, 4 + 3 will give the result of 7.
    ⁕ Subtraction (-): This operator is used to subtract one number from another. For example, 10 - 3 will give the result of 7.
    ⁕ Multiplication (*): This operator is used to multiply two or more numbers together. For example,4 * 3 will give the result of 12.
    ⁕ Division (/): This operator is used to divide one number by another. For example, 10 / 5 will give the result of 2
    ⁕ Modulus (%): This operator is used to find the remainder when one number is divided by another. For example, 10 % 3 will give the result of 1.
    Exponentiation (**): This operator is used to raise a number to a certain power. For example, 2 ** 4 will give the result of 16.

---
+++ arithmetic_operations.js
@@ -0,0 +1,15 @@
+let a = 20;
+let b = 10;
+let c;
+
+c = a+b; console.log(c);
+c = a-b; console.log(c);
+c = a*b; console.log(c);
+c = a/b; console.log(c);
+c = a%b; console.log(c);
+c = a**b; console.log(c);
+c = a**2; console.log(c);
+c = a++; console.log(c, a);
+c = ++a; console.log(c, a);
+c = --a; console.log(c, a);
+c = a--; console.log(c, a);
