3f0ce2da72aded8f2c7216e6dedd048b5c4113e1
ThiyaguAntony <antonythiyagu29@gmail.com>
2025-02-28 00:33:55 +0530

    String Functions in JavaScript
    String functions are used in computer programming languages to manipulate a string or query information about a string (some do both). Most programming languages that have a string datatype will have some string functions although there may be other low-level ways within each language to handle strings directly. .JavaScript has many built-in string functions that can be used to manipulate strings. Some common string functions include
    
    ⁕  length: returns the number of characters in a string
    ⁕  toUpperCase(): returns the string in all uppercase letters
    ⁕  toLowerCase(): returns the string in all lowercase letters
    ⁕  indexOf(substring): returns the index of the first occurrence of the specified substring, or -1 if the substring is not found
    ⁕  lastIndexOf(substring): returns the index of the last occurrence of the specified substring, or -1 if the substring is not found
    ⁕  substring(start, end): returns a part of the string between the specified start and end indexes
    ⁕  split(separator): splits a string into an array of substrings based on the specified separator
    ⁕  replace(old, new): replaces all occurrences of a specified string with another string
    ⁕  concat(string2): concatenates two strings

---
+++ string_functions.js
@@ -0,0 +1,55 @@
+let fname = "Thiyagu";
+let lname = "Antony";
+
+console.log(fname.length);
+
+console.log(fname.toUpperCase());
+
+console.log(fname.toUpperCase());
+
+console.log(fname.indexOf('a'));
+
+console.log(fname.lastIndexOf('i'));
+
+console.log(fname.charAt(3));
+
+console.log(fname.charCodeAt(1));
+
+// substring
+console.log(fname.substring(3,5));
+
+console.log(fname.substring(2));
+
+console.log(fname.substring(-2));
+
+// slice
+console.log(fname.slice(1));
+
+console.log(fname.slice(1,4));
+
+console.log(fname.slice(-2));
+
+console.log(fname.split('i'));
+
+console.log(fname.concat(" ",lname));
+
+console.log(fname.replace('T', 't'));
+
+var a = ['dog', 'cat', 'fox'];
+
+console.log(a.includes('dog'));
+
+var b = "variables are used to store data values ";
+
+console.log(b);
+
+console.log(b.trim());
+
+var c = "5";
+console.log(c.padStart(4,0));
+
+console.log(c.padEnd(4,0));
+
+var d ="In JavaScript, variables are used to store data values. You can declare variables using var, let, or const.";
+
+console.log(String.fromCharCode(56,80,65,80));
